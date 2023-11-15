73f84236d9db6d70e6e7ec65006d93b814a845f1
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2023-11-15 12:59:47 +0900

    update next/image使わずに画像表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/logo_wolf.png"
-        alt="Mavs Logo"
-        width={100}
-        height={142}
-        priority
-      />
+      <picture>
+        <source srcSet="/logo_wolf.webp" width="97" type="image/webp" />
+        <img src="/logo_wolf.png" alt="Mavs Logo" />
+      </picture>
     </main>
   )
 }
