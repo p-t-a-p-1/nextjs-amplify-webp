a332ee11a4b97980a31bd836c9a8c9f594ecf7c6
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2023-12-07 14:07:18 +0900

    update AVIF画像の追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <p>WebP</p>
       <picture>
         <source srcSet="/logo_wolf.webp" width="97" type="image/webp" />
+        <img src="/logo_wolf.png" alt="Mavs Logo" />
+      </picture>
+
+      <p>AVIF</p>
+      <picture>
+        <source srcSet="/logo_wolf.avif" width="97" type="image/avif" />
         <img src="/logo_wolf.png" alt="Mavs Logo" />
       </picture>
     </main>
